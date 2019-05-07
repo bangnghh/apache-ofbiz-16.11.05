f3fedf33e2ecc564c05f90578173fc2922908d37
DESKTOP-FB97H2A\Bang <bang.nghh>
2019-05-07 17:11:11 +0700

    remove module CloneSomething
    remove unused module CloneSomething

--- specialpurpose/CloneSomething/webapp/CloneSomething/WEB-INF/actions/crud/ListDemProduct.groovy
+++
@@ -1,4 +0,0 @@
-demProductTypes = delegator.findList("DemProductType", null, null, null, null, false);
-context.demProductTypes = demProductTypes;
-demProductList = delegator.findList("DemProduct", null, null, null, null, false);
-context.demProductList = demProductList;
